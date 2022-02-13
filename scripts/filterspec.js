d41347f663225f094961657c463dd44908cf5c7a
Pitohui202 <lencomputer60@gmail.com>
2022-02-13 23:41:39 +0100

    Tastatur Support

--- scripts/filterspec.js
+++ scripts/filterspec.js
@@ -1,6 +1,19 @@
 var but=document.getElementById("newdash");
+var uindat=document.getElementById("uindat");
 var val;
 but.addEventListener("click",testfornow);
+
+$(document).keydown(function ToClick(evn){
+    if (evn.keyCode==13) {
+        but.click();
+    }
+})
+
+function ToClick(evn){
+    if (evn.keyCode==13) {
+        but.click();
+    }
+};
 function updateVal(title){
 val=title.value
 };
